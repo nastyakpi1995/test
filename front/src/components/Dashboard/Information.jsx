5cc7169d6c70bed22104943df6e938b8b9f8603d
Nastya Krat <nastyakpi1995@gmail.com>
2021-12-29 12:06:12 +0200

    dashboard front

---
+++ front/src/components/Dashboard/Information.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Card} from "antd";
+
+const Information = () => {
+    return (
+        <Card hoverable style={{ width: 300, height: 300, margin: 60, justifyContent: 'center', textAlign: 'center' }}>
+            <div>users:</div>
+            <div>3</div>
+        </Card>
+    )
+}
+
+export default Information
--- front/src/components/Dashboard/index.jsx
+++ front/src/components/Dashboard/index.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import UserMainWrapper from "../common/UserMainWrapper";
+import Information from "./Information";
 
 const Dashboard = () => {
+    useEffect(() => {
 
+    }, [])
     return (
         <UserMainWrapper>
-
+            <div style={{display: 'flex', height: '100vh'}}>
+                <Information />
+                <Information />
+                <Information />
+            </div>
         </UserMainWrapper>
         )
 }
